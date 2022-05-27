330a454bd28489a150364b9e69ccc95e4f59b865
Juan <105739344+contra3000@users.noreply.github.com>
2022-05-27 12:59:20 -0300

    PyEx7.1 Openbootcamp
    Se sube solucion a consigna de ejercicio 1 del tema 7."Modulos" de Openbootcamp en codigo Python

---
+++ Ejercicio7.1/main.py
@@ -0,0 +1,18 @@
+import operaciones as op
+
+
+def main():
+
+    resuma = op.suma(5, 7, 4)
+    reresta = op.resta(20, 4)
+    remulti = op.multiplicacion(2, 8)
+    redivi = op.division(32, 2)
+
+    print(resuma)
+    print(reresta)
+    print(remulti)
+    print(redivi)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Ejercicio7.1/operaciones.py
@@ -0,0 +1,14 @@
+def suma(a, b, *args):
+    return a + b + sum(args)
+
+
+def resta(a, b):
+    return a - b
+
+
+def multiplicacion(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
